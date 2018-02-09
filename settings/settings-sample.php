56550a5f3e1705298355e058524350836d41a20b
everaldomatias <matiaseveraldo@gmail.com>
2018-02-09 09:27:39 -0200

    Adiciona arquivo modelo para conexao com o banco de dados

---
+++ settings/settings-sample.php
@@ -0,0 +1,19 @@
+<?php
+	// Duplique esse arquivo com o nome settings.php
+	// nessa mesma pasta /settings e adicione as configurações
+	// necessárias.
+	// 
+	// Lembrando que o arquivo que criar não deve ser versionado.
+
+	$post 		= "localhost";
+	$usuario	= "root";
+	$senha		= "pass";
+	$banco		= "chat";
+
+	// Iniciando a conexão
+	$conecta = mysql_connect( $host, $usuario, $senha ) OR print( mysql_error() );
+	mysql_select_db( $banco, $conecta );
+
+	if ( ! $conecta ) {
+		echo "Erro ao se conectar com o banco de dados!";
+	}
